a8c979c7382301edb2f8692c2ff19bbf717a4b58
uzinok <uzinok@yandex.ru>
2019-06-19 09:49:50 +0400

    часть js меню лендинга

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,13 @@
 window.onload = function () {
+    
+    document.addEventListener("touchstart", function() {
+        var clear_class = document.querySelectorAll(".hover");
+        for(var i = 0; i < clear_class.length; i++) {
+            clear_class[i].classList.remove("hover");
+        }
+
+    });
+
     var 
         menu = document.querySelector(".main-menu"),
         section_scroll = document.querySelectorAll(".section-js");
@@ -16,21 +25,46 @@ window.onload = function () {
 
             button_menu.setAttribute("data-title", my_attr);
             button_menu.setAttribute("href", "#" + my_id);
-            
+
+            button_menu.addEventListener("touchstart", function(e) {
+                e.preventDefault();
+
+                console.log(e);
+
+                if(this.classList.contains("hover")) {
+                    this.classList.remove("hover");
+                    my_scroll(this);
+                }
+                else {
+                    this.classList.add("hover");
+                }
+                
+            });
 
             button_menu.addEventListener("mouseover", function() {
-                this.classList.add("button-scroll");
+                this.classList.add("menu-scroll");
             });
 
             button_menu.addEventListener("mouseout", function() {
-                this.classList.remove("button-scroll");
+                this.classList.remove("menu-scroll");
             });
 
-            button_menu.addEventListener("click", function (e) {
+            button_menu.addEventListener("click", function(e) {
                 e.preventDefault();
 
-                var
-                hash = this.getAttribute("href"),
+                my_scroll(this);
+            });
+            
+            
+
+            
+        }
+
+        
+
+        function my_scroll(button_menu) {
+            var
+                hash = button_menu.getAttribute("href"),
                 height_scroll = window.pageYOffset,
                 top_section = document.querySelector(hash).getBoundingClientRect().top,
                 my_height_scroll = height_scroll + top_section;
@@ -39,13 +73,21 @@ window.onload = function () {
                     top: my_height_scroll,
                     left: 0,
                     behavior: 'smooth'
-                  });
-            });
-            
-            
-
-            
+                });
         }
+
+
+
+        var my_scroll_btn = document.querySelectorAll(".button-scroll");
+
+        for(var i = 0; i < my_scroll_btn.length; i++) {
+        
+            my_scroll_btn[i].addEventListener("click", function(e) {
+                    e.preventDefault();
+    
+                    my_scroll(this);
+                });
+        };
 
 
 };

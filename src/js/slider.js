f04caf2c3e2cd580321c982957ee19a7e1ca3e59
uzinok <uzinok@yandex.ru>
2020-01-29 19:18:01 +0400

    slider resize

--- src/js/slider.js
+++ src/js/slider.js
@@ -17,29 +17,73 @@ function slider(sliderList) {
     sliderWrap.appendChild(sliderWrapList);
     sliderWrapList.appendChild(sliderList);
 
-    // кнопки вперд/назад
+    function createElements() { // кнопки вперд/назад
 
-    let sliderNext = createButton('slider__next', sliderWrap, 'следующий слайд');
+        let sliderNext = createButton('slider__next', sliderWrap, 'следующий слайд');
 
-    let sliderPrew = createButton('slider__prew', sliderWrap, 'предидущий слайд');
+        let sliderPrew = createButton('slider__prew', sliderWrap, 'предидущий слайд');
 
-    // пагер
-    let pager = document.createElement('div');
-    pager.classList.add('pager');
-    sliderWrap.appendChild(pager);
+        // пагер
+        let pager = document.createElement('div');
+        pager.classList.add('pager');
+        sliderWrap.appendChild(pager);
 
-    // кнопки пагера
-    for (let i = 0; i < sliderItems.length; i++) {
-        let pagerButton = createButton('pager-button', pager, 'показать слайд номер: ' + ( i + 1), i);
-        sliderItems[i].setAttribute('data-slide', i);
+        // кнопки пагера
+        for (let i = 0; i < sliderItems.length; i++) {
+            let pagerButton = createButton('pager-button', pager, 'показать слайд номер: ' + (i + 1), i);
+            sliderItems[i].setAttribute('data-slide', i);
+        }
+
+        function createButton(buttonClass, buttonParent, ariaText, i) {
+            let elem = document.createElement('button');
+            elem.classList.add(buttonClass);
+            elem.setAttribute('aria-label', ariaText);
+            if (i != undefined) elem.setAttribute('data-slide', i);
+            buttonParent.appendChild(elem);
+            return elem;
+        }
     }
 
-    function createButton(buttonClass, buttonParent, ariaText, i) {
-        let elem = document.createElement('button');
-        elem.classList.add(buttonClass);
-        elem.setAttribute('aria-label', ariaText);
-        if(i != undefined)elem.setAttribute('data-slide', i);
-        buttonParent.appendChild(elem);
-        return elem;
+    function removeElements() {
+        parentSlider.querySelector('.slider__next').remove();
+        parentSlider.querySelector('.slider__prew').remove();
+        parentSlider.querySelector('.pager').remove();
+    }
+
+    checkCreateElements()
+
+    // resize
+    window.addEventListener('resize', function () {
+        windowResize();
+    })
+
+    // безопасный resize
+    let check;
+    function windowResize() {
+        clearTimeout(check);
+
+        check = setTimeout(function () {
+
+            // фенкции для resize
+            checkCreateElements();
+        }, 100)
+    }
+
+    // проверка наличия элементов
+    function checkCreateElements() {
+
+        let widthWindow = window.innerWidth;
+
+        if (widthWindow >= 985 && sliderItems.length == 3) {
+            if (parentSlider.querySelector('.slider__next')) {
+                console.log('hi!');
+                removeElements();
+            }
+        } else {
+            if (!(parentSlider.querySelector('.slider__next'))) {
+                createElements();
+            }
+        }
+
     }
 }

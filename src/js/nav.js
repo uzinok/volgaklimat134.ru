96e2f8ad5060604586150cb8fc485aadf20186a4
uzinok <uzinok@yandex.ru>
2020-03-01 16:45:56 +0400

    правка nav.js

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,6 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let nav = document.querySelector('.nav');
-    document.querySelector('.nav__button').addEventListener('click', function() {
-        nav.classList.toggle('nav--open')
-    })
+document.addEventListener('DOMContentLoaded', function () {
+    if (document.querySelector('.nav')) {
+        let nav = document.querySelector('.nav');
+        document.querySelector('.nav__button').addEventListener('click', function () {
+            nav.classList.toggle('nav--open')
+        })
+    }
 })

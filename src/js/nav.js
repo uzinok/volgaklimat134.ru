856ae8b19d6e873be7cec17347b957a7177ec06a
uzinok <uzinok@yandex.ru>
2020-05-21 20:05:34 +0400

    21/05/12

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
     if (document.querySelector('.nav')) {
         let nav = document.querySelector('.nav');
-        document.querySelector('.nav__button').addEventListener('click', function () {
-            nav.classList.toggle('nav--open')
-        })
+
+        let navButton = document.createElement('button');
+        navButton.classList.add('nav__button');
+        navButton.setAttribute('aria-label', 'Открыть меню');
+        nav.appendChild(navButton);
+
+
+        if (document.querySelector('.nav')) {
+            navButton.addEventListener('click', function () {
+                nav.classList.toggle('nav--open')
+            })
+        }
     }
 })
